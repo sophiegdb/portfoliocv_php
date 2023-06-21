dff0adb3d54d8922b4e035debaa0853440e48dac
Sophie de Breuvand <sophie.breuvand@le-campus-numerique.fr>
2023-06-21 10:48:14 +0200

    tous mes fichiers cv

---
+++ cv_php/header.php
@@ -0,0 +1,10 @@
+<?php
+$metaTittle = "CV SOPHIE DE BREUVAND";
+?>
+<head>
+    <meta charset="UTF-8">
+
+    <title> <?= $metaTittle ?></title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
---
+++ cv_php/hobby.php
@@ -0,0 +1,37 @@
+<?php>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hobby</title>
+    <link rel="stylesheet" href="hobby.css">
+</head>
+<body>
+    <h1>La peinture abstraite</h1>
+    <br>
+    <br>
+        <p>La peinture abstraite est une forme d’art contemporain. Non figuratif, cet art utilise des codes 
+            différents dans sa réalisation ainsi que les sens et l’imagination de ceux qui la regardent.
+            Le but n’est pas de copier la réalité, ni d’en créer une, mais de créer une expérience visuelle
+            et de ressentir des émotions.</p>
+        <br>
+        <p>J'ai adoré créer des peintures abstraites pour ma maison, elles symbolisent une émotion que je ne
+            verbalise pas forcément, ou changent complétement l'esthetique d'une pièce.
+            Il n'y a pas besoin d'avoir une technique particulière, ni beaucoup de materiel, on peut prendre 
+            des échantillons de peinture pour les murs, peindre aves ses doigts, ajouter des jeux de textures 
+            avec par exemple du papier cellophane, etc... La créativitée n'a aucune limite !
+        </p>
+    <br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/glxNklCpU-Y" title="YouTube video 
+        player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+        picture-in-picture; web-share" allowfullscreen></iframe>
+    <br>
+    <br>
+        <a href="index.html">Page 1</a>
+        <a href="contact.html">Page 3</a>
+
+    </body>
+</html>
+<?php>
